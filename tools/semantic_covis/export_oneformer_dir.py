cae781a39d119f3de4c8db140cb214a4256544d1
zhouh02 <13305819151@163.com>
2026-05-17 19:27:15 +0800

    code2

--- tools/semantic_covis/export_oneformer_dir.py
+++ tools/semantic_covis/export_oneformer_dir.py
@@ -290,7 +290,9 @@ def export_oneformer_dir(
         else:
             rel_stem = img_stem
 
-        npz_path = npz_dir / f"{rel_stem.replace('/', '_').replace('\\\\', '_')}.npz"
+        # npz_path = npz_dir / f"{rel_stem.replace('/', '_').replace('\\\\', '_')}.npz"
+        safe_stem = rel_stem.replace("/", "_").replace("\\", "_")
+        npz_path = npz_dir / f"{safe_stem}.npz"
         json_path = json_dir / f"{rel_stem.replace('/', '_').replace('\\\\', '_')}.json"
 
         # Skip existing
